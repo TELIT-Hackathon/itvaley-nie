876c29b14b930a270e2100a33c045629ced57230
Jan <lengyel1999@gmail.com>
2022-12-03 14:58:51 +0100

    Start server

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { tagsApi } from "./routes/tags-api.mjs";
+import express from "express";
+import { tagsApi } from "./Routes/tags-api.mjs";
 
 const baseRouter = new Router();
 baseRouter.use((req, res, next) => {
@@ -11,3 +12,12 @@ baseRouter.use((req, res, next) => {
 });
 
 baseRouter.use("/api/tags", tagsApi);
+
+
+const app = express();
+app.use('/',baseRouter);
+
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
