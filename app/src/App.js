465fccbf35d5a19616f58dc92da6369084adac05
Matúš Semančík <matus.semancik@gmail.com>
2022-12-03 17:55:24 +0100

    router

--- app/src/App.js
+++ app/src/App.js
@@ -4,28 +4,32 @@ import Dashboard from './component/homeScreen';
 import LogIn from './component/signIn.js';
 import SignUp from './component/signUp.js';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello madafaka
-        </a>
-      </header> */}
-      {/* <LogIn/> */}
-      <SignUp/>
-      {/*<Dashboard/>*/}
-    </div>
-  );
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Typography } from '@mui/material';
+
+	function App() {
+	return (
+		<BrowserRouter className="App">
+		<Routes>
+			<Route path="/">
+				<Route index element={
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Hello madafaka
+					</a>
+				} />
+				<Route path="home" element={<Dashboard/>} />
+				<Route path="register" element={<SignUp/>} />
+				<Route path="login" element={<LogIn/>} />
+				<Route path="*" element={<Typography>NENI TAKE</Typography>} />
+			</Route>
+		</Routes>
+		</BrowserRouter>
+	);
+	}
 
 export default App;
