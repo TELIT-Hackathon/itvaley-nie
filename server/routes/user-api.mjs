4c69f9facf45daef83b198b238115f0c5e3a4b79
Matúš Semančík <matus.semancik@gmail.com>
2022-12-03 16:33:00 +0100

    fix

--- server/routes/user-api.mjs
+++ server/routes/user-api.mjs
@@ -34,7 +34,7 @@ userApi.post('/login', async (req, res) => {
             res.json({token})
         }
         else {
-            throw
+            throw new Error()
         }
     }
     catch(err) {
