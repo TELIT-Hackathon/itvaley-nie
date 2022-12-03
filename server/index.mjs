00206098ff6b49af69601f96bae8d814e631c1c4
Jan <lengyel1999@gmail.com>
2022-12-03 14:44:55 +0100

    Added tags api

--- server/index.mjs
+++ server/index.mjs
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { tagsApi } from "./routes/tags-api.mjs";
 
-const baseRouter = new Router()
+const baseRouter = new Router();
 baseRouter.use((req, res, next) => {
     console.log(`${new Date().toISOString()} - ${req.originalUrl}`);
 
     //TODO add login verify
 
     next();
-})
+});
+
+baseRouter.use("/api/tags", tagsApi);
