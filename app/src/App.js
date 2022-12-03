5470dd782c3ec4366c9c2ae310e8569a3cf170bd
Matúš Semančík <matus.semancik@gmail.com>
2022-12-03 19:34:30 +0100

    sadasds

--- app/src/App.js
+++ app/src/App.js
@@ -15,28 +15,28 @@ const theme = createTheme();
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-		<BrowserRouter className="App">
-		<Routes>
-			<Route path="/">
-				<Route index element={
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Hello madafaka
-					</a>
-				} />
-				<Route path="home" element={<HomeScreen/>} />
-        <Route path="contacts" element={<Contacts/>} />
-				<Route path="dashboard" element={<Dashboard/>} />
-				<Route path="register" element={<SignUp/>} />
-				<Route path="login" element={<LogIn/>} />
-				<Route path="*" element={<Typography>NENI TAKE</Typography>} />
-			</Route>
-		</Routes>
-		</BrowserRouter>
+			<BrowserRouter className="App">
+			<Routes>
+				<Route path="/">
+					<Route index element={
+						<a
+							className="App-link"
+							href="https://reactjs.org"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Hello madafaka
+						</a>
+					} />
+					<Route path="home" element={<HomeScreen/>} />
+					<Route path="contacts" element={<Contacts/>} />
+					<Route path="dashboard" element={<Dashboard/>} />
+					<Route path="register" element={<SignUp/>} />
+					<Route path="login" element={<LogIn/>} />
+					<Route path="*" element={<Typography>NENI TAKE</Typography>} />
+				</Route>
+			</Routes>
+			</BrowserRouter>
 		</ThemeProvider>
 	);
 }
