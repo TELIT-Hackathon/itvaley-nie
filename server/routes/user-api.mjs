b23254fdaf5464f070ec94bf846d4bed0854d5b4
Matúš Semančík <matus.semancik@gmail.com>
2022-12-03 16:05:42 +0100

    merge

--- server/routes/user-api.mjs
+++ server/routes/user-api.mjs
@@ -1,7 +1,13 @@
-import { Router } from "express"
-import User from "../Models/User.mjs"
+import { Router } from 'express'
+import User from '../Models/User.mjs'
+import Session from '../Models/Session.mjs'
+import crypto from 'crypto'
 
 export const userApi = new Router()
+
+const hashPassword = password => crypto.createHash('sha256').update(password).digest('hex')
+const generateToken = () => crypto.randomBytes(64).toString('hex')
+
 userApi.get('/me', (req, res) => {
     res.send(req.user);
 });
@@ -13,9 +19,32 @@ userApi.get('/:id', async (req, res) => {
     const user = await User.findOne({id:req.params.id})
     res.json(user)
 });
-userApi.post('/login', (req, res) => {
-    res.send(`GET data about ${req.params.id}`)
-});
+userApi.post('/login', async (req, res) => {
+    try {
+        const username = req.body.username || ''
+        const password = req.body.password || ''
+
+        const user = await User.findOne({username: username})
+        if(user && user.password === hashPassword(password)) {
+            const token = generateToken()
+
+            const session = new Session({user: user.id, token})
+            await session.save()
+            
+            res.json({token})
+        }
+        else {
+            throw
+        }
+    }
+    catch(err) {
+        res.status(500).send(err)
+    }
+})
+userApi.post('/logout', (req, res) => {
+    Session.findOne({user: req.user.id}).remove().exec()
+    res.end()
+})
 userApi.post('/register', async (req, res) => {
     const data = req.body
     console.log(data)
